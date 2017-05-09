87328a8f4e1c1ef8a18d7ace4777cd332187fb57
kramarmm <kramar.mariya@mail.ru>
2017-05-09 19:51:44 +0300

    changes port to process.env.PORT

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');  
 const app = express();  
-const port = 3000;
 
 var favicon = require('serve-favicon');
 app.use(favicon(path.join(__dirname, 'public/', 'favicon.ico'))); 
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port, (err) => {  
+app.listen(process.env.PORT || 3000, (err) => {  
   if (err) {
     return console.log('something bad happened', err)
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log(`server is listening`)
 })
